02f23927126ff50bdbb08ac69b897094d033154e
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2020-10-03 21:27:47 +0600

    Argon SCSS added

---
+++ src/components/HeaderPlugin/HeaderPlugin.vue
@@ -0,0 +1,71 @@
+<template>
+  <b-navbar class="navbar-container border-bottom">
+    <b-form class="d-none d-md-block">
+      <b-form-group id="nav-search" class="mb-0">
+        <b-form-input size="sm" placeholder="Search.."></b-form-input>
+      </b-form-group>
+    </b-form>
+    <b-navbar-nav class="ml-auto">
+      <b-list-group>
+        <b-list-item>
+          <b-avatar size="5rem"></b-avatar>
+        </b-list-item>
+      </b-list-group>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+import {
+  BNavbar,
+  BForm,
+  BFormGroup,
+  BFormInput,
+  BNavbarNav,
+  BAvatar,
+  BListGroup,
+  BListItem,
+} from "bootstrap-vue";
+export default {
+  name: "Header",
+  components: {
+    BNavbar,
+    BForm,
+    BFormGroup,
+    BFormInput,
+    BNavbarNav,
+    BAvatar,
+    BListGroup,
+    BListItem,
+  },
+};
+</script>
+
+<style scoped></style>
+
+<!--template>
+  <div
+    id="header"
+    class="navbar navbar-expand-lg navbar-fixed navbar-height navbar-container navbar-bordered"
+  >
+    <div class="navbar-nav-wrap justify-content-between">
+      &lt;!&ndash;<div class="navbar-brand-wrapper"></div>&ndash;&gt;
+      <div class="navbar-nav-wrap-content-left">
+        <div class="d-none d-md-block">
+          <form action="" class="position-relative">
+            <div class="input-group">
+              <input type="text" />
+              <a href="">submit</a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="navbar-nav-wrap-content-right">
+        <ul class="navbar-nav align-items-center flex-row">
+          <li class="nav-item">notification</li>
+          <li class="nav-item">avatar</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>-->
---
+++ src/components/SidebarPlugin/SideBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav
+    class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light sidenav-bg p-0"
+    id="sidenav-main"
+  >
+    <b-img
+      :src="require('../../assets/img/logo/Devport-logo.png')"
+      fluid
+    ></b-img>
+    <div class="pt-4 ml-4">
+      <ul class="list-unstyled">
+        <li v-for="item in sidebarItems" :key="item.label" class="mb-3">
+          <router-link :to="item.link">
+            <span style="color: #00d7ab">
+              <font-awesome-icon :icon="item.icon" class="mr-2 fa-lg" />
+            </span>
+            <span class="font-weight-500 fs-0 text-white">{{ item.label }}</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { BImg } from "bootstrap-vue";
+export default {
+  name: "SideBar",
+  components: {
+    BImg,
+  },
+  data() {
+    return {
+      sidebarItems: [
+        {
+          label: "Profile",
+          icon: "cog",
+          link: "/profile",
+        },
+        {
+          label: "Settings",
+          icon: "cog",
+          link: "/settings",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
