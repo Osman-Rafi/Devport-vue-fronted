f4068e68a45c2281442ae0c5626135f56709d7e7
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2021-02-07 14:21:31 +0600

    package(vuex-persistedstate): get vuex state after reload

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 import auth from "./modules/auth";
 
 Vue.use(Vuex);
@@ -7,4 +8,5 @@ export default new Vuex.Store({
   modules: {
     auth,
   },
+  plugins: [createPersistedState()],
 });
