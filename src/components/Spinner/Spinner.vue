62c8cf3d31cc4fbea2b9822c0c5da2526c0cf770
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2021-02-18 14:38:52 +0600

    feat(spinner): created

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-if="loading" class="d-flex justify-content-center">
+      <b-spinner
+        :type="spinnerType"
+        :variant="spinnerVariant"
+        :style="spinnerSize"
+      ></b-spinner>
+    </div>
+    <div v-else>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BSpinner } from "bootstrap-vue";
+export default {
+  name: "Spinner",
+  components: {
+    BSpinner,
+  },
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+    spinnerType: {
+      type: String,
+    },
+    spinnerVariant: {
+      type: String,
+      default: "secondary",
+    },
+    spinnerSize: {
+      type: String,
+      default: "width:3rem; height:3rem",
+    },
+  },
+};
+</script>
